36b09dea7e733f44e006a6013e1e628819c94828
unknown <akshaypanchal67@gmail.com>
2020-10-28 12:01:42 -0400

    added react-redux and redux node packages to the project

---
+++ src/manageStore/manageStore.js
@@ -0,0 +1,17 @@
+import {createStore} from 'redux';
+
+
+const defaultState = {
+    todoItems: []
+}
+
+
+
+const setTodoListItemData = ()=>{
+
+}
+
+const store = createStore(setTodoListItemData);
+
+
+export default store;
